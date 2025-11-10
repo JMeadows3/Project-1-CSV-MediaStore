2a38e5803ec31a05446a0b3f1bfa4df7b163f5a9
Jmeadows3 <jon_meadows@ymail.com>
2025-11-09 22:22:29 -0600

    finished Project 1 with CSV file Edited print statements to be more precise
    Signed-off-by: Jmeadows3 <jon_meadows@ymail.com>

--- src/Driver.java
+++ src/Driver.java
@@ -19,16 +19,16 @@ public class Driver {
 
     public static void printAnalytics(Manager manager) { //print method to clean up main
 
-        System.out.println(manager.numMediaType(Movies.class) + " MOVIES"); // number of movies
-        System.out.println(manager.numMediaType(TVShows.class) + " TV SHOWS"); // number of TV Shows
-        System.out.println(manager.numMediaType(VideoGames.class) + " Video Games"); // number of Video Games
-        System.out.println(manager.numMediaType(MusicAlbums.class) + " Music Albums"); // number of Music Albums
-        System.out.println("\"" + manager.shortestDuration(MusicAlbums.class) + "\"" + " is the Shortest album"); // shortest Music Album
-        System.out.println("\"" + manager.shortestDuration(Movies.class) + "\"" + " it the Shortest movies"); // shortest Movie
-        System.out.println(manager.getCount() + " Products"); // number of product in the file
-        System.out.println("\"" + manager.avgRating() + "\"" + " is the Avg Film rating"); // avg age rating of Movies/Shows
-        System.out.println("\"" + manager.mostPopular(VideoGames.class) + "\"" + " is the Most Popular Video Game"); // Most sold Video Game
-        System.out.println("\"" + manager.mostPopular(MusicAlbums.class) + "\"" + " is the Most Popular Music Album"); // Most Sold Music Album
-        System.out.println("\"" + manager.oldestProduct() + "\"" + " is the Oldest Product"); // Oldest overall Product
+        System.out.println(manager.getCount() + " Total Products"); // number of product in the file
+        System.out.println(manager.numMediaType(Movies.class) + " - Movies Available"); // number of movies
+        System.out.println(manager.numMediaType(TVShows.class) + " - TV Shows Available"); // number of TV Shows
+        System.out.println(manager.numMediaType(VideoGames.class) + " - Video Games Available"); // number of Video Games
+        System.out.println(manager.numMediaType(MusicAlbums.class) + " - Music Albums Available"); // number of Music Albums
+        System.out.println("\"" + manager.shortestDuration(MusicAlbums.class) + "\"" + " - is the Title of the Shortest Music Album"); // shortest Music Album
+        System.out.println("\"" + manager.shortestDuration(Movies.class) + "\"" + " - it the Title of the Shortest Movie"); // shortest Movie
+        System.out.println("\"" + manager.avgRating() + "\"" + " - is the Average Film rating"); // avg age rating of Movies/Shows
+        System.out.println("\"" + manager.mostPopular(VideoGames.class) + "\"" + " - is the Most Popular Video Game"); // Most sold Video Game
+        System.out.println("\"" + manager.mostPopular(MusicAlbums.class) + "\"" + " - is the Most Popular Music Album"); // Most Sold Music Album
+        System.out.println("\"" + manager.oldestProduct() + "\"" + " - is the Oldest Product"); // Oldest overall Product
     }
 }
